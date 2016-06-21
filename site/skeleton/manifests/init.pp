ddcfe0df20f3b68e598a050d7bca3f95177614b8
jarnol7 <julie.arnold@walmart.com>
2016-06-21 13:30:11 -0500

    Addd ensure

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,12 +1,14 @@
 class skeleton {
   file { '/etc/skel':
-#    ensure => ???,   # what value should go here?
+    ensure => 'directory'
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
   }
   
   # add a resource to manage /etc/skel/.bashrc
-
+  file { '/etc/skel/.basshrc': 
+  ensure => file,
+  #not done yet
 
 }
