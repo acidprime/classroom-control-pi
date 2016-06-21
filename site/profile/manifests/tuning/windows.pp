ecb064ed46b9a73d10d5b55877b7c29faa8c729b
Zack Smith <acidprime@wallcity.org>
2016-06-21 15:36:53 -0500

    Update windows.pp

--- site/profile/manifests/tuning/windows.pp
+++ site/profile/manifests/tuning/windows.pp
@@ -1,10 +1,14 @@
 class profile::tuning::windows {
     registry::value { 'KeepAliveInterval':
       key    => 'HKLM\SYSTEM\CurrentControlSet\Services\AFD\Parameters',
-      value  => 'KeepAliveInterval',
       data   => '1',
     }
-
-  # Add more registry resources as directed
-  
+     registry::value { 'TcpMaxDataRetransmissions':
+      key    => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
+      data   => '5',
+    }
+     registry::value { 'TcpTimedWaitDelay':
+      key    => 'HKLM\SYSTEM\CurrentControlSet\Services\TCPIP\Parameters',
+      data   => '0x0000001e',
+    }
 }
