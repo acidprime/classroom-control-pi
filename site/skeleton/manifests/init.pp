d43073249a32e4d5ccb6cac65e13455263f6f737
sentientbn <jaclark3s@gmail.com>
2016-06-21 13:37:44 -0500

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -10,6 +10,9 @@ class skeleton {
   
   file {'/etc/skel/.bashrc':
     ensure => file,
-   
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0440',
+    source  => 'puppet:///modules/skeleton/bashrc',
   }
 }
