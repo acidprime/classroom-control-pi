95d111dfb54460307ccc2d99507d356b6fbd968d
Zack Smith <acidprime@wallcity.org>
2016-06-22 12:38:57 -0500

    Update init.pp

--- site/dsciis/manifests/init.pp
+++ site/dsciis/manifests/init.pp
@@ -8,11 +8,11 @@ class dsciis {
 
   # Translate these DSC Powershell resources into Puppet code below
   dsc_windowsfeature { 'iis':
-    dsc_ensure    = 'present',
-    dsc_name      = 'Web-Server',
+    dsc_ensure    => 'present',
+    dsc_name      => 'Web-Server',
   }
   
-  dsc_windowsfeature { 'iisscriptingtools'
+  dsc_windowsfeature { 'iisscriptingtools':
     dsc_ensure    => 'present',
     dsc_name      => 'Web-Scripting-Tools',
   }
