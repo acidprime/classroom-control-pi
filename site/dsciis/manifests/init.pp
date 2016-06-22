88c4156723e54fede37a4196eec1bad60f875ef6
GHB0794521 <bizzobb@gmail.com>
2016-06-22 12:28:46 -0500

    Update init.pp

--- site/dsciis/manifests/init.pp
+++ site/dsciis/manifests/init.pp
@@ -1,26 +1,28 @@
 class dsciis {
 
   # TODO: add the correct name to this resource so DSC can trigger reboots
-  reboot { '???':
+  reboot { 'dsc_reboot':
     when    => pending,
     timeout => 15,
   }
 
   # Translate these DSC Powershell resources into Puppet code below
-#  WindowsFeature iis {
-#    Ensure    = 'Present'
-#    Name      = 'Web-Server'
-#  }
+  WindowsFeature iis {
+    Ensure    = 'Present'
+    Name      = 'Web-Server'
+  }
   
-#  WindowsFeature iisscriptingtools {
-#    Ensure    = 'Present',
-#    Name      = 'Web-Scripting-Tools',
-#  }
+  WindowsFeature iisscriptingtools {
+    Ensure    = 'Present',
+    Name      = 'Web-Scripting-Tools',
+  }
   
   # The index file is managed as a native Puppet file resource.
   file { 'C:/inetpub/wwwroot/index.html':
     ensure  => 'file',
     source  => 'puppet:///modules/dsciis/index.html',
+    
+    require => Dsc_windowsfeature['iis'],
     # TODO: what relationship should go here?
     
   }
