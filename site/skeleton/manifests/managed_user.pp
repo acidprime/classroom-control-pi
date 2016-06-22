8d478e58c0c7cda96b888ba0827092cbfc8898b9
GHB0794521 <bizzobb@gmail.com>
2016-06-22 09:07:27 -0500

    Update managed_user.pp

--- site/skeleton/manifests/managed_user.pp
+++ site/skeleton/manifests/managed_user.pp
@@ -1,7 +1,8 @@
 define skeleton::managed_user (
+  $password,
   $home = undef,
   # TODO: Add a password parameter
-  
+
 ) {
   if $home {
     $homedir = $home
@@ -43,7 +44,10 @@ define skeleton::managed_user (
     }
 
     # TODO: Add a file resource to manage "${homedir}/.bashrc"
-
+  file { "${homedir}/.bashrc":
+    ensure => file,
+    source => 'puppet:///modules/skeleton/bashrc',
+    }
   }
 
   # Puppet will evaluate these resources in the proper order because it's smart
@@ -51,6 +55,7 @@ define skeleton::managed_user (
 
   user { $name:
     ensure     => present,
+    password => $password,
     managehome => true,
     # TODO: Pass the password parameter to this resource
     
